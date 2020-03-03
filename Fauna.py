dea564154c357baa495bb7d1c75f2c7d96110622
Christian M. Rahmel <35943405+OmeganKryist@users.noreply.github.com>
2020-03-03 00:46:42 -0800

    Update Fauna.py

--- Fauna.py
+++ Fauna.py
@@ -1,8 +1,97 @@
+# START FILE
+#=======================================================================
+# GENERAL DOCUMENTATION ------------------------------------------------
+""" 
+"""
+
+# ADDITIONAL DOCUMENTATION ---------------------------------------------
+
+# Modification History:
+# - 3 Mar 2020: File Created
+
+# Notes:
+# - Written for Python 3.7
+# - To test simply run the file in the canopy distribution of python
+#  or through some other IDE like visual studio or spyder
+# - Documentation style inspired by CSS 458 professor Johnny Lin
+
+#=======================================================================
+# PROGRAM IMPORTS ------------------------------------------------------
+import numpy as nu
+
+# PROGRAM CONSTANTS ----------------------------------------------------
+# User modifiable
+
+VAR = 0                     # var meaning
+
+# PROGRAM GLOBALS ------------------------------------------------------
+# Not User Modifiable
+
+var = 0                     # var meaning
+
+#=======================================================================
+# CLASS: Fauna ---------------------------------------------------------
 class Fauna:
-    """description of class"""
-
+    """ Description:
+    
+        Variables: 
+        -var: 
+    
+        Methods: 
+    """
+    # MEATHOD: INIT ----------------------------------------------------
+    def __init__(self):
+        """ Description: Class constructor
+            
+    
+            Variables: 
+            -var: 
+    
+            Output: 
+        """
+        return 1
+    
+# CLASS: Herbivore -----------------------------------------------------
 class Herbivore(Fauna):
-    """description of class"""
-
+    """ Description:
+    
+        Variables: 
+        -var: 
+    
+        Methods: 
+    """
+    # MEATHOD: INIT ----------------------------------------------------
+    def __init__(self):
+        """ Description: Class constructor
+            
+    
+            Variables: 
+            -var: 
+    
+            Output: 
+        """
+        return 1
+    
+# CLASS: Carnivore -----------------------------------------------------
 class Carnivore(Fauna):
-    """description of class"""
+    """ Description:
+    
+        Variables: 
+        -var: 
+    
+        Methods: 
+    """
+    # MEATHOD: INIT ----------------------------------------------------
+    def __init__(self):
+        """ Description: Class constructor
+            
+    
+            Variables: 
+            -var: 
+    
+            Output: 
+        """
+        return 1
+
+#=======================================================================
+# END FILE
