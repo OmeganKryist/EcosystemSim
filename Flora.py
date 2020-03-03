15048c62e6881ef3fa43ad7c609b4bddc05fe6d9
Christian M. Rahmel <35943405+OmeganKryist@users.noreply.github.com>
2020-03-03 00:48:17 -0800

    Update Flora.py

--- Flora.py
+++ Flora.py
@@ -1,3 +1,60 @@
-class Flora:
-    """description of class"""
+# START FILE
+#=======================================================================
+# GENERAL DOCUMENTATION ------------------------------------------------
+""" 
 
+    See code documentation for specifics on code functionality
+"""
+
+# ADDITIONAL DOCUMENTATION ---------------------------------------------
+
+# Modification History:
+# - 3 Mar 2020: File Created
+
+# Notes:
+# - Written for Python 3.7
+# - To test simply run the file in the canopy distribution of python
+#  or through some other IDE like visual studio or spyder
+# - Documentation style inspired by CSS 458 professor Johnny Lin
+
+#=======================================================================
+# PROGRAM IMPORTS ------------------------------------------------------
+import numpy as nu
+
+# PROGRAM CONSTANTS ----------------------------------------------------
+# User modifiable
+
+VAR = 0                     # var meaning
+
+# PROGRAM GLOBALS ------------------------------------------------------
+# Not User Modifiable
+
+var = 0                     # var meaning
+
+# PROGRAM SCRIPT -------------------------------------------------------
+# Driver code for program
+
+#=======================================================================
+# CLASS: Flora ---------------------------------------------------------
+class Flora:
+    """ Description:
+    
+        Variables: 
+        -var: 
+    
+        Methods: 
+    """
+    # MEATHOD: INIT ----------------------------------------------------
+    def __init__(self):
+        """ Description: Class constructor
+            
+    
+            Variables: 
+            -var: 
+    
+            Output: 
+        """
+        return 1
+
+#=======================================================================
+# END FILE
