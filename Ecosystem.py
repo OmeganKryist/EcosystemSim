6361fce06234315565f1f9ad65123b43280707df
WilliamTaing <53840760+WilliamTaing@users.noreply.github.com>
2020-03-04 21:55:30 -0800

    updated ecosystem.py

--- Ecosystem.py
+++ Ecosystem.py
@@ -55,100 +55,99 @@ class EcoSystem:
             Output: 
         """
         #Grid Size variables
-        length = 50
-        width = 50
+        self.length = 50
+        self.width = 50
         #These grids are booleans(0/1) for when animals are at a location
-        plant_grid = nu.zeros(length, width)
-        herbivore_grid = nu.zeros(length, width)
-        carnivore_grid = nu.zeros(length, width)
+        self.plant_grid = nu.zeros((self.length, self.width))
+        self.herbivore_grid = nu.zeros((self.length, self.width))
+        self.carnivore_grid = nu.zeros((self.length, self.width))
         
         #These lists hold every individual plant and animal
-        plant_list = []
-        herbivore_list = []
-        carnivore_list = []
+        self.plant_list = []
+        self.herbivore_list = []
+        self.carnivore_list = []
         
         self.initPlants()
-        return 1
+        return
 
 # FUNCTION: FUNC -------------------------------------------------------
-def func():
-    """ Description:
+    def func():
+        """ Description:
+            
         
-    
-        Variables: 
-        -var: 
-    
-        Output: 
-    """
-    return 2
-
-def randomWalk(self, Fauna):
-    """ Description:
-        Pass an animal and it will walk within the borders of the grid.
-        (Moore's Neighborhood)
-    
-        Variables: 
-        -Fauna.position: current location of fauna
-        -moveX: Array of x positions fauna can move
-        -moveY: Array of y positions fauna can move
-    
-        Output: 
-            Fauna has changed locations
-    """
-    
-    #Moore Neighborhood walk
-    #Directions in x-axis
-    moveX = nu.array([1, 1, 1, 0, 0, -1, -1, -1])
-    #Fauna.position[0] current x location
-    moveX = moveX + Fauna.position[0]
-    
-    #Directions in y-axis
-    moveY = nu.array([1, 0, -1, 1, -1, 1, 0, -1])
-    #Fauna.position[1] current y location
-    moveY = moveY + Fauna.position[1]
-    
-    #Check Borders
-    violation = nu.where(nu.logical_or( \
-    nu.logical_or(moveY >= self.length, moveY < 0),\
-    nu.logical_or(moveX >= self.width, moveX < 0)))
-    
-    #Delete areas that cross borders
-    nu.delete(moveX, violation)
-    nu.delete(moveY, violation)
-    
-    index = nu.arange(len(moveX))
-    nu.shuffle(index)
-    
-    #moveX and moveY may need to switch 
-    Fauna.position(moveX[index[0]], moveY[index[0]])
-    
-    return
-    
-def initPlants(self):
-    """ Description:
+            Variables: 
+            -var: 
         
-        Populates the plant list by determining if a plant grows in an area
-        using a random number.
+            Output: 
+        """
+        return 2
     
-        Variables: 
-        -plantChance: Chance a plant will grow in a grid space.
-    
-        Output: plant_list and plant_grid is populated with Grass objects.
-    """
-    #For initializing grass
-    plantChance = 0.85
-    
-    #Test every grid space for plant growth
-    for i in range(self.length):
-        for j in range(self.width):
-            #Test for growth
-            if nu.random.uniform(0,1) <= plantChance:
-                #Make a grass plant
-                newPlant = fo.Grass()
-                self.plant_grid[i,j] = 1
-                self.plant_list.append(newPlant)
-    #Grid and list should now be initialized
-    return
+    def randomWalk(self, Fauna):
+        """ Description:
+            Pass an animal and it will walk within the borders of the grid.
+            (Moore's Neighborhood)
+        
+            Variables: 
+            -Fauna.position: current location of fauna
+            -moveX: Array of x positions fauna can move
+            -moveY: Array of y positions fauna can move
+        
+            Output: 
+                Fauna has changed locations
+        """
+        
+        #Moore Neighborhood walk
+        #Directions in x-axis
+        moveX = nu.array([1, 1, 1, 0, 0, -1, -1, -1])
+        #Fauna.position[0] current x location
+        moveX = moveX + Fauna.position[0]
+        
+        #Directions in y-axis
+        moveY = nu.array([1, 0, -1, 1, -1, 1, 0, -1])
+        #Fauna.position[1] current y location
+        moveY = moveY + Fauna.position[1]
+        
+        #Check Borders
+        violation = nu.where(nu.logical_or( \
+        nu.logical_or(moveY >= self.length, moveY < 0),\
+        nu.logical_or(moveX >= self.width, moveX < 0)))
+        
+        #Delete areas that cross borders
+        nu.delete(moveX, violation)
+        nu.delete(moveY, violation)
+        
+        index = nu.arange(len(moveX))
+        nu.shuffle(index)
+        
+        #moveX and moveY may need to switch 
+        Fauna.position(moveX[index[0]], moveY[index[0]])
+        
+        return
+        
+    def initPlants(self):
+        """ Description:
+            
+            Populates the plant list by determining if a plant grows in an area
+            using a random number.
+        
+            Variables: 
+            -plantChance: Chance a plant will grow in a grid space.
+        
+            Output: plant_list and plant_grid is populated with Grass objects.
+        """
+        #For initializing grass
+        plantChance = 0.85
+        
+        #Test every grid space for plant growth
+        for i in range(self.length):
+            for j in range(self.width):
+                #Test for growth
+                if nu.random.uniform(0,1) <= plantChance:
+                    #Make a grass plant
+                    newPlant = fo.Grass()
+                    self.plant_grid[i,j] = 1
+                    self.plant_list.append(newPlant)
+        #Grid and list should now be initialized
+        return
 #=======================================================================
 # END FILE
-
