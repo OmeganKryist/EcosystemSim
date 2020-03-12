2e3211fd41ee21c4dc329238e770f3c365cad91a
Christian M. Rahmel <35943405+OmeganKryist@users.noreply.github.com>
2020-03-12 00:51:20 -0700

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -24,88 +24,90 @@ import Ecosystem as sim
 # PROGRAM CONSTANTS ___________________________________________________________
 # User modifiable
 
-# ECOSYSTEM
-GRID_X = 50                     # var meaning
-GRID_Y = 50
+# ECOSYSTEM FILE
+GRID_X = 50                     # Grid length
+GRID_Y = 50                     # Grid Width
 
-NUM_SIMS = 10
-NUM_MONTHS = 2
-NUM_WEEKS = 4
-NUM_DAYS = 7
-WEEKS_PER_MONTH = 4
-DAYS_PER_WEEK = 7
-HOURS_PER_DAY = 24
+# Sim execution itterations
+NUM_SIMS = 10                   # Number of Sims to run for each 
+NUM_MONTHS = 2                  # Number of Months (if sim is by months)
+NUM_WEEKS = 4                   # Number of Weeks (if sim is by weeks)
+NUM_DAYS = 7                    # Number of Days (if sim is by Days)
+
+# Time units (sim update by hour)
+WEEKS_PER_MONTH = 4             # Number of weeks in a month
+DAYS_PER_WEEK = 7               # Number of days in a week
+HOURS_PER_DAY = 24              # Number of hours in a day
 
 # effects lighting
-MAX_CLOUDS = 10
-CLOUD_CHANCE = 0.6
-OVERCAST_CHANCE = 0.2
-RAIN_CHANCE = 0.8
+MAX_CLOUDS = 10                 # Max number of clouds at a time
+CLOUD_CHANCE = 0.6              # Chance a cloud spawns
+OVERCAST_CHANCE = 0.2           # Chance for overcast weather (no visable sun)
+RAIN_CHANCE = 0.8               # Chance for Rain
 
 # effects water
-LAKE_SPREAD = 4
-HAS_LAKE = True # bool
-POND_SPREAD = 1
-NUM_PONDS = 0
+LAKE_SPREAD = 4                 # Size of lake
+HAS_LAKE = True                 # Determins if the sime has a central lake
+POND_SPREAD = 1                 # Size of ponds
+NUM_PONDS = 0                   # Number of possible ponds
 
 # effects temp
-WATER_TEMP = -2
-LIGHT_TEMP = 2
-NATRUAL_TEMP = 10
-MAX_TEMP = 32
-MIN_TEMP = -32
+WATER_TEMP = -2                 # Impact water has on location temp
+LIGHT_TEMP = 2                  # Impact light has on location temp
+NATRUAL_TEMP = 10               # Natrual temperture offset 
+MAX_TEMP = 32                   # Maximum possible temp
+MIN_TEMP = -32                  # Minimum possible temp
 
-DISSIPATION_RATE = 0.9
-DISSIPATION_SPREAD = 0.8
-SCENT_SPREAD = 1
+# effects scent
+DISSIPATION_RATE = 0.9          # Dispation rate of the animal's scent trail
+DISSIPATION_SPREAD = 0.8        # Dispation value to cells around scent trail
+SCENT_SPREAD = 1                # how far the scent spreads from trail 
 
-PLANT_CHANCE = 0.9
-PLANT_REPOP_CHANCE = 0.1
-ENERGY_ABSORB_FACTOR = 1000
-WATER_ABSORB_FACTOR = 1000
-PLANT_UNITS_TO_EAT = 2
+# effects plants
+PLANT_CHANCE = 0.9              # Chance a cell initalizes with a plant
+PLANT_REPOP_CHANCE = 0.1        # Chance a plant repopulates a cell during sim 
+ENERGY_ABSORB_FACTOR = 1000     # Scaling value for plant photosynthsis
+WATER_ABSORB_FACTOR = 1000      # Scaling value for plant drinking
+PLANT_UNITS_TO_EAT = 2          # Number of units animals eat from plants
 
-RABBITS_PER_BURROW = 5 # must be less than 9
-MAX_RABBITS = 100
-NUM_BURROWS = 5
+# effects  animals
+RABBITS_PER_BURROW = 5          # Number of rabbits that spawn from a burrow
+MAX_RABBITS = 100               # Maximum allowed rabbits in sim
+NUM_BURROWS = 5                 # Number of burrows to generate in sim
+NUM_FOXES = 3                   # Number of foxes to initalize 
+MOVE_CHANCE = 0.5               # Chance an animal chooses to move randomly
 
-NUM_FOXES = 3
+# FAUNA FILE
+FAUNA_ENERGY_PERCENT = 0.3      # Percent of max energy goten from consume
+FAUNA_WATER_PERCENT = 0.1       # Percent of max water goten from consume
 
-MOVE_CHANCE = 0.5
+HUNGRY_PERCENT = 0.9            # Fauna hungry threshold percent 
+STARVE_PERCENT = 0.5            # Fauna food death threshold percent 
 
-# FAUNA
-FAUNA_ENERGY_PERCENT = 0.3
-FAUNA_WATER_PERCENT = 0.1
+THIRSTY_PERCENT = 0.4           # Fauna thrist threshold percent 
+DESICCATE_PERCENT = 0.2         # Fauna water death threshold percent
 
-HUNGRY_PERCENT = 0.9
-STARVE_PERCENT = 0.5
+COLD_OFFSET = -10               # Fauna cold threshold offset 
+FROZE_OFFSET = -20              # Fauna chill death threshold offset 
 
-THIRSTY_PERCENT = 0.4
-DESICCATE_PERCENT = 0.2
+HOT_OFFSET = 10                 # Fauna hot threshold offset 
+BOILED_OFFSET = 20              # Fauna heat death threshold offset 
 
-COLD_OFFSET = -10
-FROZE_OFFSET = -20
+ENERGY_MOVE_FACTOR = 6          # Scales move cost by max energy divid this
+ENERGY_WAIT_REDUCE = 2          # Energy cost reduction from moving (by divid)
+WATER_MOVE_FACTOR = 8           # Scales move cost by max water divid this
+WATER_WAIT_REDUCE = 2           # Water cost reduction from moving (by divid)
 
-HOT_OFFSET = 10
-BOILED_OFFSET = 20
+TEMP_TRANSFER = 0.2             # Percent of temp difference to impact fauna
 
-ENERGY_MOVE_FACTOR = 6
-ENERGY_WAIT_REDUCE = 2
-WATER_MOVE_FACTOR = 8
-WATER_WAIT_REDUCE = 2
+EXTRA_FOX_STEPS = 3             # Number of extra moves the foxes can make
 
-TEMP_TRANSFER = 0.2
-
-EXTRA_FOX_STEPS = 3
-
-# FLORA
-FLORA_ENERGY_PERCENT = 0.1
-FLORA_WATER_PERCENT = 0.05
+# FLORA FILE
+FLORA_ENERGY_PERCENT = 0.1      # Percent of max energy goten from consume
+FLORA_WATER_PERCENT = 0.05      # Percent of max water goten from consume
 
 # PROGRAM GLOBALS _____________________________________________________________
 # Not User Modifiable
-
-var = 0                     # var meaning
 
 #==============================================================================
 # END FILE
