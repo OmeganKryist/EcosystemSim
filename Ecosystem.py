ab34d37778fbd3f5dabbd72eab54d1b09aee534b
WilliamTaing <53840760+WilliamTaing@users.noreply.github.com>
2020-03-11 20:20:45 -0700

    Update Ecosystem.py

--- Ecosystem.py
+++ Ecosystem.py
@@ -31,6 +31,7 @@ GRID_X = 50                     # var meaning
 GRID_Y = 50
 
 NUM_DAYS = 10
+NUM_SIMS = 10
 
 # effects lighting
 MAX_CLOUDS = 10
@@ -801,6 +802,96 @@ def func():
     return 2
 
 #=======================================================================   
+# Analysis Functions --------------------------------------------------
+def anRabToPlant(perBurrow, numBurrows):
+    #Parameters passed
+    global RABBITS_PER_BURROW
+    RABBITS_PER_BURROW = perBurrow # must be less than 9
+    global NUM_BURROWS
+    NUM_BURROWS = numBurrows
+    
+    beginningPlants = []
+    endPlants = []
+    
+    print("Rabbits per Burrow: ", perBurrow)
+    print("Burrows:", numBurrows)
+    print("TotalRabbits:", perBurrow * numBurrows)
+    
+    for j in range(NUM_SIMS):
+        eco = EcoSystem()           #Initialize Ecosystem
+        beginningPlants.append(len(eco.plant_list))
+        for i in range(NUM_DAYS):
+            eco.runADay()
+        endPlants.append(len(eco.plant_list))
+        
+    avgBeginning = sum(beginningPlants) / len(beginningPlants)
+    avgEnd = sum(endPlants) / len(endPlants)
+    print("# Simulations:", NUM_SIMS)
+    print("Average plants at beginning:", avgBeginning)
+    print("Average plants at end:", avgEnd)
+    print("Average plant difference:", avgBeginning - avgEnd, "\n")
+    
+def anLakeToRab(spread):
+    global LAKE_SPREAD
+    LAKE_SPREAD = spread
+    print("Lake Spread:", spread)
+    endRabbits = []
+    for j in range(NUM_SIMS):
+        eco = EcoSystem()
+        for i in range(NUM_DAYS):
+            eco.runADay()  
+        endRabbits.append(len(eco.herbivore_list))
+        
+    avgEnd = sum(endRabbits) / len(endRabbits)
+    print("# Simulations:", NUM_SIMS)
+    print("Average rabbits at end:", avgEnd)
+    
+def anDissipationtoRab(dis):
+    global DISSIPATION_RATE
+    DISSIPATION_RATE = dis
+    
+    print("Dissipation Rate:", dis)
+    endRabbits = []
+    
+    for j in range(NUM_SIMS):
+        eco = EcoSystem()
+        for i in range(NUM_DAYS):
+            eco.runADay()  
+        endRabbits.append(len(eco.herbivore_list))
+    
+    avgEnd = sum(endRabbits) / len(endRabbits)
+    print("# Simulations:", NUM_SIMS)
+    print("Average rabbits at end:", avgEnd, "\n")
+    
+def anFoxToRab(fox):
+    global NUM_FOXES
+    NUM_FOXES = fox
+    
+    print("Foxes:", fox)
+    endRabbits = []
+    dayExtinct = []
+    
+    lastDay = 0
+    for j in range(NUM_SIMS):
+        lastDay = 10
+        eco = EcoSystem()
+        for i in range(NUM_DAYS):
+            eco.runADay()  
+            #Check if all rabbits are dead
+            if len(eco.herbivore_list) == 0:
+                if i < lastDay:
+                    lastDay = i
+        dayExtinct.append(lastDay)
+        endRabbits.append(len(eco.herbivore_list))
+        
+    avgEnd = sum(endRabbits) / len(endRabbits)
+    avgDay = sum(dayExtinct) / len(dayExtinct)
+    print("# Simulations:", NUM_SIMS)
+    
+    print("Average rabbits at end:", avgEnd)
+    print("Average day eliminated:", avgDay, "\n")
+
+
 # PROGRAM SCRIPT ------------------------------------------------------
 # Driver code for program
 
