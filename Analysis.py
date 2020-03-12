c40ef4b73e43ca26e768af484abe0dbc68094734
Christian M. Rahmel <35943405+OmeganKryist@users.noreply.github.com>
2020-03-11 23:27:26 -0700

    Update Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -25,11 +25,13 @@ import Variables as const
 #======================================================================
 # Analysis Functions --------------------------------------------------
 def anRabToPlant(perBurrow, numBurrows):
+    # grab values for restoring values
+    hold1 = const.RABBITS_PER_BURROW
+    hold2 = const.NUM_BURROWS
+    
     #Parameters passed
-    global RABBITS_PER_BURROW
-    RABBITS_PER_BURROW = perBurrow # must be less than 9
-    global NUM_BURROWS
-    NUM_BURROWS = numBurrows
+    const.RABBITS_PER_BURROW = perBurrow # must be less than 9
+    const.NUM_BURROWS = numBurrows
     
     beginningPlants = []
     endPlants = []
@@ -52,9 +54,14 @@ def anRabToPlant(perBurrow, numBurrows):
     print("Average plants at end:", avgEnd)
     print("Average plant difference:", avgBeginning - avgEnd, "\n")
     
+    # restore values
+    const.RABBITS_PER_BURROW = hold1
+    const.NUM_BURROWS = hold2
+    
 def anLakeToRab(spread):
-    global const.LAKE_SPREAD
-    LAKE_SPREAD = spread
+    hold = const.LAKE_SPREAD # grab value for restoring value
+    
+    const.LAKE_SPREAD = spread
     print("Lake Spread:", spread)
     endRabbits = []
     for j in range(const.NUM_SIMS):
@@ -67,10 +74,12 @@ def anLakeToRab(spread):
     print("# Simulations:", const.NUM_SIMS)
     print("Average rabbits at end:", avgEnd)
     
-def anDissipationtoRab(dis):
-    global const.DISSIPATION_RATE
-    DISSIPATION_RATE = dis
+    const.LAKE_SPREAD = hold # restore value
     
+def anDissipationtoRab(dis):
+    hold = const.DISSIPATION_RATE # grab value for restoring value
+    
+    const.DISSIPATION_RATE = dis
     print("Dissipation Rate:", dis)
     endRabbits = []
     
@@ -84,10 +93,12 @@ def anDissipationtoRab(dis):
     print("# Simulations:", const.NUM_SIMS)
     print("Average rabbits at end:", avgEnd, "\n")
     
-def anFoxToRab(fox):
-    global const.NUM_FOXES
-    NUM_FOXES = fox
+    const.DISSIPATION_RATE = hold # restore value
     
+def anFoxToRab(fox):
+    hold = const.NUM_FOXES # grab value for restoring value
+    
+    const.NUM_FOXES = fox
     print("Foxes:", fox)
     endRabbits = []
     dayExtinct = []
@@ -111,6 +122,8 @@ def anFoxToRab(fox):
     
     print("Average rabbits at end:", avgEnd)
     print("Average day eliminated:", avgDay, "\n")
+    
+    const.NUM_FOXES = hold # restore value
     
 # SIMULATION TEST -------------------------------------------------------------
 def testSim():
