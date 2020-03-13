22b46540db8986fc7f695326f60955be512c0e0c
WilliamTaing <53840760+WilliamTaing@users.noreply.github.com>
2020-03-12 18:50:28 -0700

    Update Fauna.py
    comments

--- Fauna.py
+++ Fauna.py
@@ -44,32 +44,32 @@ class Fauna:
     INIT_WATER_MIN = None          # initialization minimum for water
     INIT_WATER_MAX = None        # initialization range for water
     
-    energy = None
-    max_energy = None
-    move_energy_cost = None
-    wait_energy_cost = None
-    energy_value = None
-    hungry = None
-    eat_amount = None
-    starve = None
+    energy = None                # current fauna energy
+    max_energy = None            # maximum fauna energy
+    move_energy_cost = None      # energy cost of moving
+    wait_energy_cost = None      # energy cost of waiting
+    energy_value = None          # energy provided to animal if consumed
+    hungry = None                # threshold for being hungry
+    eat_amount = None            # maximum amount of energy gained by eating 
+    starve = None                # threshold for dying of hunger
     
-    water = None
-    max_water = None
-    move_water_cost = None
-    wait_water_cost = None
-    water_value = None
-    thirsty = None
-    drink_amount = None
-    desiccate = None
+    water = None                 # current fauna water
+    max_water = None             # maximum fauna water
+    move_water_cost = None       # water cost of moving
+    wait_water_cost = None       # water cost of waiting
+    water_value = None           # water provided to animal if consumed
+    thirsty = None               # threshold for being thirsty
+    drink_amount = None          # maximum amount of water gained by drinking
+    desiccate = None             # threshold for dying of thirst
     
-    temp = None
-    cold = None
-    hot = None
-    froze = None
-    boiled = None
+    temp = None                  # current temperature
+    cold = None                  # threshold for being cold
+    hot = None                   # threshold for being hot
+    froze = None                 # threshold for dying of cold
+    boiled = None                # threshold for dying of heat
     
-    position = None
-    alive = None
+    position = None              # current position in environment
+    alive = None                 # alive status flag
 
     # MEATHOD: init -----------------------------------------------------------
     def __init__(self, y, x):
@@ -80,27 +80,32 @@ class Fauna:
             -x: x position
             -y: y position
         """
-        
+        #Initializes energy and water through uniform distribution
         self.energy = nu.random.uniform(self.INIT_ENERGY_MIN,\
                                         self.INIT_ENERGY_MAX)
         self.water = nu.random.uniform(self.INIT_WATER_MIN,\
                                        self.INIT_WATER_MAX)
         
+        #Initializes temperature depending on natural temperature and environment
         self.temp = self.natural_temp
         self.cold = self.natural_temp + const.COLD_OFFSET
         self.froze = self.natural_temp + const.FROZE_OFFSET
         self.hot = self.natural_temp + const.HOT_OFFSET
         self.boiled = self.natural_temp + const.BOILED_OFFSET
         
+        #Initializes energy and water values for when fauna is consumed
         self.energy_value = self.max_energy * const.FAUNA_ENERGY_PERCENT
         self.water_value = self.max_water * const.FAUNA_WATER_PERCENT
         
+        #Initializes threshold for when fauna becomes hungry and thirsty
         self.hungry = self.max_energy * const.HUNGRY_PERCENT
         self.thirsty = self.max_water * const.THIRSTY_PERCENT
         
+        #Initializes threshold for when fauna dies from hunger or thirst
         self.starve = self.max_energy * const.STARVE_PERCENT
         self.desiccate = self.max_water * const.DESICCATE_PERCENT
         
+        #Initializes energy and water costs of taking an action
         self.move_energy_cost = ((self.max_energy / const.ENERGY_MOVE_FACTOR)\
                                  / const.HOURS_PER_DAY)
         self.wait_energy_cost = self.move_energy_cost\
@@ -109,18 +114,24 @@ class Fauna:
                                 / const.HOURS_PER_DAY)
         self.wait_water_cost = self.move_water_cost / const.WATER_WAIT_REDUCE 
         
+        #Initializes location in environment
         self.position = [y, x]
-        self.alive = True
+        self.alive = True           #Set fauna as alive
 
     # MEATHOD: eat ------------------------------------------------------------
     def eat(self, amount):
-        """ Description: updates the fauna's energy based on a given 
-                         amount
+        """ Description: Updates the fauna's energy based on a given 
+                         amount. If there is more food than the maximum
+                         amount the fauna can eat, it will just eat
+                         the max amount.
     
             Variables: 
             -self: instance of class
             -amount: variable change to energy level
                      assumed to be positive
+                     
+            Postconditions: energy is increased depending on amount of food
+                            available when eat was called.
         """
         self.energy += min(amount, self.eat_amount)
 
@@ -144,6 +155,10 @@ class Fauna:
             -self: instance of class
             -x: x position
             -y: y position
+            
+            Postconditions:
+            Fauna's position data is updated. Temperature is updated depending
+            on location. Energy and Water are reduced by the move cost.
         """
         if(tempValue < self.temp):
             self.temp = max(self.temp -\
@@ -162,12 +177,25 @@ class Fauna:
     
             Variables: 
             -self: instance of class
+            
+            Postconditions:
+            Energy and Water are reduced by the wait cost.
         """
         self.energy -= self.wait_energy_cost
         self.water -= self.wait_water_cost
     
     # MEATHOD: consumed -------------------------------------------------------    
     def consumed(self):
+        """ Description: Fauna has been eaten and it returns the amount of energy and water
+                         it provides.
+        
+            Variables:
+            -self: instance of class
+           
+            Postconditions:
+            Returns an energy and water value depending on fauna
+            
+        """
         self.alive = False # it's been eaten
         # return array of energy and water values to add to predators
         return [self.energy_value, self.water_value]
@@ -181,57 +209,57 @@ class Fauna:
 
             Output: a boolean value indicating the life of the fauna
         """
+        #Compare current fauna condition to the thresholds
         if(self.alive):
-            if(self.energy < self.starve):
+            if(self.energy < self.starve):      #Energy too low
                 self.alive = False
-            elif(self.water < self.desiccate):
+            elif(self.water < self.desiccate):  #Water too low
                 self.alive = False
-            elif(self.temp <= self.froze):
+            elif(self.temp <= self.froze):      #Temp too low
                 self.alive = False
-            elif(self.temp >= self.boiled):
+            elif(self.temp >= self.boiled):     #Temp too high
                 self.alive = False
         return self.alive
     
     # MEATHOD: isHerbivore ----------------------------------------------------
     def isHerbivore(self):
+        """ Description: Returns whether a fauna is an herbivore"""
         return False
     
     # MEATHOD: isCarnivore ----------------------------------------------------
     def isCarnivore(self):
+        """ Description: Returns whether a fauna is a carnivore"""
         return False 
     
 # CLASS: Herbivore ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class Herbivore(Fauna):
-    """ Description:
-    
-        Variables: 
-        -var: 
+    """ Description: Herbivore class for fauna. An animal that eats plants.
+                     Inherits Fauna
     """
     
     # MEATHOD: isHerbivore ----------------------------------------------------
     def isHerbivore(self):
+        """ Description: Returns whether a fauna is an herbivore"""
         return True
 
 # CLASS: Carnivore ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 class Carnivore(Fauna):
-    """ Description:
-    
-        Variables: 
-        -var:  
+    """ Description: Carnivore class for fauna. An animal that eats other
+                     animals.
     """
     
     # MEATHOD: isCarnivore ----------------------------------------------------    
     def isCarnivore(self):
+        """ Description: Returns whether a fauna is a carnivore"""
         return True
     
 # CLASS: Rabbit +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++        
 class Rabbit(Herbivore):
     
     """ Description: Rabbit Class
-        I read that rabbits need to eat 105 calories a day to maintain
-        optimal health so their max energy is 1000 and they die within 6 days
-        of not eating and 3 of not drinking. A rabbit should gain about 105
-        energy when eating every day.
+                     Rabbits roam around the environment, eating plants and looking for 
+                     water if they are thirsty. They are spawned from rabbit burrows.
+                     Inherits Herbivore
             
     
             Variables: 
@@ -242,17 +270,22 @@ class Rabbit(Herbivore):
     INIT_WATER_MIN = 900          # initialization minimum for water
     INIT_WATER_MAX = 1000        # initialization range for water
     
-    max_energy = 1000
-    eat_amount = 100
+    max_energy = 1000            # maximum amount of energy contained
+    eat_amount = 100             # maximum amount of energy recovered from eating
     
-    max_water = 1000
-    drink_amount = 500
+    max_water = 1000             # maximum amount of water contained
+    drink_amount = 500           # maximum amount of water recovered from drinking
     
-    natural_temp = 5
+    natural_temp = 5             # natural temperature
     
 # CLASS: Fox ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++        
 class Fox(Carnivore, Herbivore):
-    """ Description: Modeled after the red fox, a native animal to Washington.
+    """ Description: Fox Class
+                     Foxes are both carnivores and herbivores. They will hunt
+                     down rabbits by following their scent. They can also eat
+                     plants to survive.
+                     Inherits Herbivore, Carnivore
+                     
     
     """
     
@@ -261,13 +294,13 @@ class Fox(Carnivore, Herbivore):
     INIT_WATER_MIN = 1250         # initialization minimum for water
     INIT_WATER_MAX = 1500       # initialization range for water
     
-    max_energy = 5000
-    eat_amount = 1000
+    max_energy = 5000           # maximum amount of energy contained
+    eat_amount = 1000           # maximum amount of energy restored from eating
     
-    max_water = 1500
-    drink_amount = 750
+    max_water = 1500            # maximum amount of water contained
+    drink_amount = 750          # maximum amount of water restored from drinking
     
-    natural_temp = 10
+    natural_temp = 10           # natural temperature
 
     # MEATHOD: init -----------------------------------------------------------
     def __init__(self, y, x):
@@ -277,28 +310,36 @@ class Fox(Carnivore, Herbivore):
             -self: instance of class
             -x: x position
             -y: y position
+            
+            PostConditions: 
+            One of the fauna objects are created.
         """
-        
+        #Initializes energy and water through uniform distribution
         self.energy = nu.random.uniform(self.INIT_ENERGY_MIN,\
                                         self.INIT_ENERGY_MAX)
         self.water = nu.random.uniform(self.INIT_WATER_MIN,\
                                        self.INIT_WATER_MAX)
         
+        #Initializes temperature depending on natural temperature and environment
         self.temp = self.natural_temp
         self.cold = self.natural_temp + const.COLD_OFFSET
         self.froze = self.natural_temp + const.FROZE_OFFSET
         self.hot = self.natural_temp + const.HOT_OFFSET
         self.boiled = self.natural_temp + const.BOILED_OFFSET
         
+        #Initializes energy and water values for when fauna is consumed
         self.energy_value = self.max_energy * const.FAUNA_ENERGY_PERCENT
         self.water_value = self.max_water * const.FAUNA_WATER_PERCENT
         
+        #Initializes threshold for when fauna becomes hungry and thirsty
         self.hungry = self.max_energy * const.HUNGRY_PERCENT
         self.thirsty = self.max_water * const.THIRSTY_PERCENT
         
+        #Initializes threshold for when fauna dies from hunger or thirst
         self.starve = self.max_energy * const.STARVE_PERCENT
         self.desiccate = self.max_water * const.DESICCATE_PERCENT
         
+        #Initializes energy and water costs of taking an action
         self.move_energy_cost = ((self.max_energy / const.ENERGY_MOVE_FACTOR)\
                                  / const.HOURS_PER_DAY) / const.EXTRA_FOX_STEPS
         self.wait_energy_cost = self.move_energy_cost\
@@ -307,7 +348,8 @@ class Fox(Carnivore, Herbivore):
                                 / const.HOURS_PER_DAY) / const.EXTRA_FOX_STEPS
         self.wait_water_cost = self.move_water_cost / const.WATER_WAIT_REDUCE 
         
+        #Initializes location in environment
         self.position = [y, x]
-        self.alive = True
+        self.alive = True      #Set fauna as alive
 #==============================================================================
 # END FILE
